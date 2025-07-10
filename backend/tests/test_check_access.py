a7e8da592b5dff05c5e21a0bd445877180144fb7
nikolaospapa3 <nikolaospapa3@gmail.com>
2025-07-10 12:25:26 +0300

    [workflows] add templates for environment variables

--- backend/tests/test_check_access.py
+++ backend/tests/test_check_access.py
@@ -1,9 +1,13 @@
 # using pytest and FastAPI's TestClient to test the check_access endpoint
 import unittest
 
+from dotenv import load_dotenv
 from fastapi.testclient import TestClient
 
 from backend.monolith.app import app
+
+env_path = ".env"
+load_dotenv(dotenv_path=env_path)
 
 TestClient = TestClient(app)
 
